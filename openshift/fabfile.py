8aadf33435ac8446ffe23fbc6b3497455caca9ba
FengXi <westwin@gmail.com>
2016-10-25 21:36:37 +0800

    feat(openshift): install openshift

--- openshift/fabfile.py
+++ openshift/fabfile.py
@@ -23,6 +23,8 @@ def install_openshift():
 
     clone_openshift_ansible_repo()
     install_docker()
+    config_docker_storage()
+    config_container_logs()
 
 
 def install_base_pkg():
@@ -60,13 +62,30 @@ def install_docker():
     """
     install docker 
     """
-    def _config():
-        """
-        docker config
-        """
-        cmd = """sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --insecure-registry 172.30.0.0/16"' \
-/etc/sysconfig/docker"""
-        run(cmd)
 
     run("yum install -y docker")
-    _config()
+
+    #docker config
+    cmd = """sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --insecure-registry 172.30.0.0/16"' /etc/sysconfig/docker"""
+    run(cmd)
+
+    #start docker
+    run("systemctl enable docker")
+    run("systemctl start docker")
+
+def config_docker_storage():
+    """
+    TODO: config docker storage
+    """
+    pass
+
+def config_container_logs(max_size='1M', max_file=3):
+    """
+    managing container logs.
+    TODO: paramerize
+    """
+    cmd = """sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --insecure-registry 172.30.0.0/16 --log-opt max-size=1M --log-opt max-file=3"' /etc/sysconfig/docker"""
+    run(cmd)
+    
+    run("systemctl restart docker")
+    
