541f673ba728507dba08b37f7a9062b5832cc6b5
Feng Xi <xifeng@nationsky.com>
2017-02-23 11:07:01 +0800

    feat(fabric): config bj internal registry.

--- docker/fabfile.py
+++ docker/fabfile.py
@@ -15,7 +15,7 @@ from fabric.contrib.files import append, exists
 
 def install_yum_repo():
     """install docker-engine yum repo"""
-    run("yum-config-manager --add-repo https://docs.docker.com/engine/installation/linux/repo_files/centos/docker.repo")
+    sudo("yum-config-manager --add-repo https://docs.docker.com/engine/installation/linux/repo_files/centos/docker.repo")
 
 def install():
     """
@@ -24,26 +24,26 @@ def install():
     """
     install_yum_repo()
 
-    run("yum install -y docker-engine")
-    run("yum update -y ")
+    sudo("yum install -y docker-engine")
+    sudo("yum update -y ")
 
-    config_registry_mirror(restart=False)
-    config_storage(restart=False)
+    #config_registry_mirror(restart=False)
+
+    use_bj_registry()
 
     #start docker
-    run("systemctl enable docker")
-    run("systemctl start docker")
+    _restart()
 
 def install_compose():
     #cmd = """curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose"""
 
     cmd = """curl -L "https://github.com/docker/compose/releases/download/1.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose"""
-    run(cmd)
-    run("chmod +x /usr/local/bin/docker-compose")
+    sudo(cmd)
+    sudo("chmod +x /usr/local/bin/docker-compose")
 
     #docker compose completion
     cmd = """curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose"""
-    run(cmd)
+    sudo(cmd)
 
 def uninstall():
     """
@@ -65,16 +65,68 @@ def config_registry_mirror(restart=True):
     sudo(cmd)
     _restart(restart)
 
+def use_bj_registry():
+    """install beijing internal docker registry """
+    dns_resolve()
+    install_bj_cert()
+    config_bj_registry()
+
+def install_bj_cert(registry="registry.nscloud.local:5005", cert_name="esdp_bj_registry.crt"):
+    """
+    install registry ssl certificate. the registry do not starts with scheme
+    """
+    scheme,fqdn = _parse_scheme(registry)
+    sudo("openssl s_client -connect %s <<<'' | openssl x509 -out /etc/pki/ca-trust/source/anchors/%s" % (fqdn, cert_name))
+    sudo("update-ca-trust enable")
+    sudo("systemctl restart docker")
+
+def config_bj_registry(pull_registry="https://registry.nscloud.local:5005",push_registry="https://registry.nscloud.local:5006", user="admin", password="admin123"):
+    """
+    config registry pull and push registry. suppose pull/push registry use same cert and same auth info.
+    """
+    basic_auth = ""
+    if user and password:
+        basic_auth = "%s:%s@" % (user, password)
+
+    scheme,fqdn = _parse_scheme(pull_registry)
+
+    registry_with_auth = "%s//%s%s" % (scheme, basic_auth, fqdn)
+    cmd = """curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s %s""" % registry_with_auth
+    sudo(cmd)
+
+    #perform a docker login for push
+    cmd = """docker login %s -u %s -p %s""" % (push_registry, user, password)
+    sudo(cmd)
+
+    _restart()
+
+def dns_resolve(fqdn="registry.nscloud.local", ip="172.16.10.231"):
+    """
+    config registry fqdn and ip mapping in case of dns can not resolve
+    NOTE: the dns server 192.168.30.202 in bj can resolve this fqdn
+    """
+    with settings(warn_only=True):
+        ping = """ping -c 3 %s""" %  fqdn
+        ret = run(ping)
+        if ret.return_code ==0:
+            print "yes. dns can resolve."
+        else:
+            print "no. dns can no resolve."
+            sudo("echo %s   %s >> /etc/hosts" % (ip,fqdn))
+
+def _parse_scheme(url):
+    """
+    parse an url to return scheme and remaining part.
+    """
+    splits = url.split("""//""")
+    if len(splits) == 2:
+        return splits[0], splits[1]
+    else:
+        return "https:", splits[0]
 def _restart(restart=True):
     #restart docker
     if restart:
         with settings(warn_only=True):
+            sudo("systemctl daemon-reload")
             sudo("systemctl enable docker")
             sudo("systemctl restart docker")
-
-def config_storage(restart=True):
-    """
-    TODO: config docker storage
-    """
-    pass
-
